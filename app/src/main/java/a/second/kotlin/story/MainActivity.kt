9cfc5e235885dc0c6db058dc82183571007c95e1
technexx <48839830+technexx@users.noreply.github.com>
2022-12-14 16:02:47 -0800

    proto start/stop of main thread co-routine

--- app/src/main/java/a/second/kotlin/story/MainActivity.kt
+++ app/src/main/java/a/second/kotlin/story/MainActivity.kt
@@ -16,6 +16,7 @@ import org.w3c.dom.Text
 //Todo: Use ViewModel and Coroutines.
 //Todo: Remember to SUSPEND and not BLOCK if using UI thread. runBlocking is a default CoroutineScope.
 
+@OptIn(DelicateCoroutinesApi::class)
 class MainActivity : AppCompatActivity() {
 
     @SuppressLint("MissingInflatedId")
@@ -23,11 +24,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var existenceTimerTextView : TextView = findViewById(R.id.existence_timer_textView)
-        val startStopButton : Button = findViewById(R.id.start_stop_button)
+        var job: Job = Job()
+        var routineIsActive : Boolean = false
 
-        var totalSpawnTimeInMilliseconds : Long = 0
-        var totalSpawnTimeInSeconds : Long = 0
+        var existenceTimerTextView: TextView = findViewById(R.id.existence_timer_textView)
+        val startStopButton: Button = findViewById(R.id.start_stop_button)
+
+        var totalSpawnTimeInMilliseconds: Long = 0
+        var totalSpawnTimeInSeconds: Long = 0
+
+        fun toggleStartStopButton(enabled: Boolean) {
+            if (enabled) {
+                startStopButton.isEnabled = true;
+                startStopButton.isClickable = true;
+            } else {
+                startStopButton.isEnabled = false;
+                startStopButton.isClickable = false;
+            }
+        }
 
         suspend fun testIteration() {
             val startTime = System.currentTimeMillis()
@@ -39,14 +53,31 @@ class MainActivity : AppCompatActivity() {
 
             existenceTimerTextView.setText((totalSpawnTimeInMilliseconds).toString())
 
-            Log.i("testRouting", "time is " + totalSpawnTimeInMilliseconds)
+//            Log.i("testRoutine", "value is " + totalSpawnTimeInMilliseconds)
         }
 
-        startStopButton.setOnClickListener {
-            GlobalScope.launch(Dispatchers.Main) {
-                while (isActive)
-                testIteration()
+        fun startTimeIterationCoRoutine() {
+            if (routineIsActive) {
+                job = GlobalScope.launch(Dispatchers.Main) {
+                    Log.i("testRoutine", "boolan in coroutine is " + routineIsActive)
+
+                    if (routineIsActive) {
+                        while (isActive)
+                            testIteration()
+                    }
+                }
+            } else {
+                job.cancel()
+                Log.i("testRoutine", "job cancelling!")
+            }
+        }
+
+            startStopButton.setOnClickListener {
+                routineIsActive = !routineIsActive
+
+                Log.i("testRoutine", "boolan in button click is " + routineIsActive)
+
+                startTimeIterationCoRoutine()
             }
         }
     }
-}
