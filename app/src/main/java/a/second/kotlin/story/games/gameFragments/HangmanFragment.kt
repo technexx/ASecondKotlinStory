2c66746f3044c7d8d9ff72b472174c9e7d5f73cb
technexx <48839830+technexx@users.noreply.github.com>
2022-12-27 16:44:50 -0800

    letter + word arrays added

--- app/src/main/java/a/second/kotlin/story/games/gameFragments/HangmanFragment.kt
+++ app/src/main/java/a/second/kotlin/story/games/gameFragments/HangmanFragment.kt
@@ -19,10 +19,17 @@ import androidx.recyclerview.widget.RecyclerView
 
 class HangmanFragment : Fragment() {
 
-    val mHangmanClass = Hangman()
+    val HangmanClass = Hangman()
     val gamesViewModel : ItemViewModel.GamesViewModel by activityViewModels()
+
     lateinit var keyboardGridView : GridView
     lateinit var rootView : View
+
+    var easyWordStringList : List<String> = ArrayList()
+    var mediumWordStringList : List<String> = ArrayList()
+    var hardWordStringList : List<String> = ArrayList()
+
+    var totalLettersArray : List<String> = ArrayList()
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -34,10 +41,16 @@ class HangmanFragment : Fragment() {
 
         rootView = inflater.inflate(R.layout.fragment_hangman_layout, container, false)
 
+        easyWordStringList = getString(R.string.easy_words_string).split(" ")
+        mediumWordStringList = getString(R.string.medium_words_string).split(" ")
+        hardWordStringList = getString(R.string.hard_words_string).split(" ")
+
+        totalLettersArray = HangmanClass.alphabetStringArray()
+
         keyboardGridView = rootView.findViewById(R.id.hangman_keyboard_gridView)
         keyboardGridView.numColumns = 9
 
-        val letterList: List<String> = mHangmanClass.alphabetStringArray()
+        val letterList: List<String> = HangmanClass.alphabetStringArray()
         val keyboardAdapter : Hangman.KeyboardRecyclerAdapter = Hangman.KeyboardRecyclerAdapter(requireContext(), R.layout.hangman_keyboard_adapter_views, R.id.hangman_letter, letterList)
 
         keyboardGridView.adapter = keyboardAdapter
@@ -45,7 +58,6 @@ class HangmanFragment : Fragment() {
         keyboardGridView.setOnItemClickListener { parent, view, position, id ->
             Toast.makeText(requireContext(), "At position $position", Toast.LENGTH_LONG).show()
         }
-
 
         return rootView
     }
