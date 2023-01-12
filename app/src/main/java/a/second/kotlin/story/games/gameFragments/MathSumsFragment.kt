18303c9c0f9c0e576e4b37696179f766f67245ab
technexx <48839830+technexx@users.noreply.github.com>
2023-01-12 11:52:28 -0800

    highlight toggle for grid positions

--- app/src/main/java/a/second/kotlin/story/games/gameFragments/MathSumsFragment.kt
+++ app/src/main/java/a/second/kotlin/story/games/gameFragments/MathSumsFragment.kt
@@ -4,6 +4,7 @@ import a.second.kotlin.story.ItemViewModel
 import a.second.kotlin.story.R
 import android.animation.ObjectAnimator
 import android.content.Context
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -15,6 +16,7 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import androidx.core.view.size
 import androidx.fragment.app.Fragment
@@ -81,33 +83,58 @@ class SumsCustomAdapter (context: Context, resource: Int, val fullCardList: Arra
     }
 
     class ObjectHolder() {
+        lateinit var populatedCardTextView : TextView
         lateinit var selectedCardView : CardView
         lateinit var selectedCardTextView : TextView
+
+        var cardSelectedPositionsList : ArrayList<Int> = ArrayList()
+        var cardSelectedValueList : ArrayList<String> = ArrayList()
     }
 
     private var holder = ObjectHolder()
 
-    //Todo: Our assignment of parent w/ in Matching is only in onClick, which is why it doesn't crash.
     override fun getView(position: Int, view: View?, parent: ViewGroup): View {
         val inflater = LayoutInflater.from(context)
         val rowView = inflater.inflate(R.layout.sums_adapter_views, null, true)
 
-        holder.selectedCardView = rowView.findViewById(R.id.sums_card_cardView)
-        holder.selectedCardTextView = rowView.findViewById(R.id.sums_card_textView)
-
-        holder.selectedCardTextView.setText(fullCardList[position].toString())
+        holder.populatedCardTextView = rowView.findViewById(R.id.sums_card_textView)
+        holder.populatedCardTextView.setText(fullCardList[position].toString())
 
         rowView.setOnClickListener {
             holder.selectedCardView = parent[position].findViewById(R.id.sums_card_cardView)
             holder.selectedCardTextView = parent[position].findViewById(R.id.sums_card_textView)
+
+            if (!isCardHighlighted(holder.selectedCardView)) highlightBackgroundOfCardView() else unHighlightBackgroundOfCardView()
+
+            addToCardPositionList(position)
         }
 
         return rowView
     }
 
-
     override fun getCount(): Int {
         return fullCardList.size
     }
 
+    private fun addToCardPositionList(position: Int) {
+        holder.cardSelectedPositionsList.add(position)
+    }
+
+    private fun removeFromCardPositionList(position: Int) {
+        holder.cardSelectedPositionsList.remove(position)
+    }
+
+    private fun highlightBackgroundOfCardView() {
+        holder.selectedCardView.setCardBackgroundColor(ContextCompat.getColor(context, R.color.light_teal))
+        holder.selectedCardView.isSelected = true
+    }
+
+    private fun unHighlightBackgroundOfCardView() {
+        holder.selectedCardView.setCardBackgroundColor(ContextCompat.getColor(context, R.color.white))
+        holder.selectedCardView.isSelected = false
+    }
+
+    private fun isCardHighlighted(cardView: CardView) : Boolean {
+        return cardView.isSelected
+    }
 }
